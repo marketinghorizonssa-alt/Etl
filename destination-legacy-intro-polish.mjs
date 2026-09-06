e5c6d97460e4ce339bdc33e4769e6b8d4170cbe9
marketinghorizonssa-alt <marketinghorizons.sa@gmail.com>
2026-09-06 18:59:08 +0300

    Add Europe destination intro artwork

--- destination-legacy-intro-polish.mjs
+++ destination-legacy-intro-polish.mjs
@@ -15,6 +15,7 @@ const introImages = {
   thailand: 'https://etlaala.com/wp-content/uploads/2025/06/%D8%AA%D8%A7%D9%8A%D9%84%D8%A7%D9%86%D8%AF-1-768x1273.webp',
   'thailand-2': 'https://etlaala.com/wp-content/uploads/2025/06/%D8%AA%D8%A7%D9%8A%D9%84%D8%A7%D9%86%D8%AF-1-768x1273.webp',
   'bosnia-and-herzegovina': 'https://etlaala.com/wp-content/uploads/2025/06/%D8%A7%D9%84%D8%A8%D9%88%D8%B3%D9%86%D8%A9-768x1273.webp',
+  europe: 'https://etlaala.com/wp-content/uploads/2026/06/Gemini_Generated_Image_yd0fcsyd0fcsyd0f-768x1353.png',
   turkiye: 'https://etlaala.com/wp-content/uploads/2025/06/%D8%AA%D8%B1%D9%83%D9%8A%D8%A7-768x1273.webp',
   'turkey-2': 'https://etlaala.com/wp-content/uploads/2025/06/%D8%AA%D8%B1%D9%83%D9%8A%D8%A7-768x1273.webp'
 };
